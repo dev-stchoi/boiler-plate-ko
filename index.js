660493894323e30775932234ce8aa40b54b34e64
dev-stchoi <littlebest17@naver.com>
2021-05-20 10:40:11 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox.1kjd0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+// application/x-www-from-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+// application/json
+app.use(express.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
@@ -11,7 +21,21 @@ mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox.1kjd0.m
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! ok');
+});
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({
+      success: true
+    })
+  });
 })
 
 app.listen(port, () => {
