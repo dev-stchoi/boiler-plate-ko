7204748593dfb75720f419e74282a222df75d19b
dev-stchoi <littlebest17@naver.com>
2021-05-26 15:17:55 +0900

    수업 마무리

---
+++ client/src/components/view/LoginPage/LoginPage.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
+
+const LoginPage = (props) => {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    };
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
+
+    const onSubmintHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email,
+            password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    props.history.push('/')
+                } else {
+                    alert('error');
+                }
+            });
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
+            <form style={{ display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSubmintHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default withRouter(LoginPage);
---
+++ client/src/components/view/RegisterPage/RegisterPage.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
+
+const RegisterPage = (props) => {
+
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    };
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
+
+    const onNameHandler = (event) => {
+        setName(event.currentTarget.value);
+    };
+
+    const onConfirmPasswordHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value);
+    };
+
+    const onSubmintHandler = (event) => {
+        event.preventDefault();
+
+
+        if (password !== confirmPassword) {
+            return alert('비밀번호와 비밀번호 확인은 같아야 합니다.');
+        }
+
+        let body = {
+            email,
+            password,
+            name,
+            confirmPassword
+        };
+
+        dispatch(registerUser(body))
+            .then(response => {
+                if (response.payload.success) {
+                    props.history.push('/login');
+                } else {
+                    alert('Faild to sign up');
+                }
+            });
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
+            <form style={{ display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSubmintHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>Name</label>
+                <input type="test" value={name} onChange={onNameHandler} />
+                <label>Password</label>
+                <input type="password" value={password} onChange={onPasswordHandler} />
+                <label>Confirm Password</label>
+                <input type="password" value={confirmPassword} onChange={onConfirmPasswordHandler} />
+                <br />
+                <button type="submit">
+                    회원가입
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default withRouter(RegisterPage);
